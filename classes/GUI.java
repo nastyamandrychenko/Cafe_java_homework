ea6ed4165ff859ce6b19a4cb53160762b210df82
Nastya <nastyamarichenko@gmail.com>
2023-10-04 23:06:45 +0200

    init commit gui

---
+++ classes/GUI.java
@@ -0,0 +1,15 @@
+package classes;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        JPanel panel = new JPanel();
+
+    }
+}
