6bf00a11b033faa546399539e56adb0298af6a95
Nastya <nastyamarichenko@gmail.com>
2023-10-04 23:11:46 +0200

    changed size of the frame

--- classes/GUI.java
+++ classes/GUI.java
@@ -6,7 +6,7 @@ import javax.swing.JPanel;
 public class GUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(600, 700);
+        frame.setSize(700, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         JPanel panel = new JPanel();
